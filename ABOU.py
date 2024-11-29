1af194caf818f8c78bcb9e29fd971cbc61cf18a8
Eonsciences <eonsciences@gmail.com>
2024-11-29 12:04:12 +0100

    ABOU.py

---
+++ ABOU.py
@@ -0,0 +1,9 @@
+# Demande deux nombres à l'utilisateur
+nombre1 = float(input("Entrez le premier nombre : "))
+nombre2 = float(input("Entrez le deuxième nombre : "))
+
+# Calcule la somme
+somme = nombre1 + nombre2
+
+# Affiche le résultat
+print(f"La somme de {nombre1} et {nombre2} est {somme}")
